8a8dca8f924593dc740882ecba774211fb24878f
IstoMan <suhasnaraparaju@gmail.com>
2023-10-24 16:48:58 +0530

    Added better compatibility with my remaps

--- nvim form scratch/lua/suhas/remaps.lua
+++ nvim form scratch/lua/suhas/remaps.lua
@@ -1,9 +1,27 @@
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
 vim.g.mapleader = " "
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.g.maplocalleader = " "
+
+-- Move text easily
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("x", "<leader>p", [["_dP]], opts)
 -- Seperation between system clipboard and neovim registors
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
-vim.keymap.set("n", "<leader>P", [["+P]])
+keymap({"n", "v"}, "<leader>y", [["+y]], opts)
+keymap("n", "<leader>Y", [["+Y]] ,opts)
+keymap({"n", "v"}, "<leader>p", [["+p]], opts)
+keymap("n", "<leader>P", [["+P]], opts)
+-- For tmux like navigation
+keymap("n", "<leader>%", ":vsplit<CR>" ,opts)
+keymap("n", "<leader>\"",":split<CR>", opts)
+keymap("n", "<leader>x",":close<CR>", opts)
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
