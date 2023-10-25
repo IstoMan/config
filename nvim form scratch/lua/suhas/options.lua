eeeecbdb8efd58310f314109c5aed3d26abe254d
IstoMan <suhasnaraparaju@gmail.com>
2023-10-25 17:39:38 +0530

    highlight on yank

--- nvim form scratch/lua/suhas/options.lua
+++ nvim form scratch/lua/suhas/options.lua
@@ -23,3 +23,14 @@ vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 -- So that extra comments don't come when going to the next line
 vim.cmd "autocmd BufNewFile,BufRead * setlocal formatoptions-=cro"
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
